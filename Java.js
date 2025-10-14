ba1640831346ae55691ab1f51084531add103c36
Mothi <glendalegold6@gmail.com>
2025-10-14 11:27:51 -0700

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,1 +1,16 @@
+document.querySelector('.dropdown-toggle').addEventListener('click', function() {
+  document.querySelector('.dropdown-content').classList.toggle('show');
+});
 
+// Optional: Close dropdown when clicking outside
+window.addEventListener('click', function(event) {
+  if (!event.target.matches('.dropdown-toggle')) {
+    var dropdowns = document.querySelectorAll('.dropdown-content');
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+});
